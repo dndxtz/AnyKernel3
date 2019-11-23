8728ea9282073c5a7c66aa2efb3f4794d7aa08f0
Ahmad Thoriq Najahi <najahiii@outlook.co.id>
2019-11-23 20:49:21 +0700

    AK3: Initial unified non-SAR/SAR build
    * This basic ideas are made by Rama, But
      in this case, I use it as unified non-SAR/SAR kernel zip
    
    Co-authored-by: Rama Bondan Prakoso <ramanarubp@gmail.com>
    Signed-off-by: Ahmad Thoriq Najahi <najahiii@outlook.co.id>

--- anykernel.sh
+++ anykernel.sh
@@ -25,6 +25,81 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+# Key select start
+ui_print " "
+ui_print "- Touch the screen first or press any volume key"
+
+INSTALLER=$(pwd)
+KEYCHECK=$INSTALLER/tools/keycheck
+chmod 755 $KEYCHECK
+
+keytest() {
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}
+
+choose() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while true; do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $KEYCHECK
+  $KEYCHECK
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Use name change method in TWRP"
+  fi
+}
+
+if [ -z $NEW ]; then
+  if keytest; then
+    FUNCTION=choose
+  else
+    FUNCTION=chooseold
+    ui_print " "
+    ui_print "- Vol Key Programming -"
+    ui_print "   Press Volume Up Key: "
+    $FUNCTION "UP"
+    ui_print "   UP ✓"
+    ui_print "   Press Volume Down Key: "
+    $FUNCTION "DOWN"
+    ui_print "   DOWN ✓"
+  fi
+  ui_print " "
+  ui_print "- Select Option -"
+  ui_print "   Choose build type you want:"
+  ui_print "   Volume Up = non-SAR, Volume Down = SAR"
+  if $FUNCTION; then
+    NEW=true
+  else
+    NEW=false
+  fi
+else
+  ui_print "   Option specified in zipname!"
+fi
+# Key select end
+
 ## AnyKernel install
 dump_boot;
 
@@ -34,14 +109,17 @@ compressed_image=$decompressed_image.gz
 if [ -f $compressed_image ]; then
   # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
   if [ -d $ramdisk/.backup ]; then
-    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
     $bin/magiskboot --decompress $compressed_image $decompressed_image;
     $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
     $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
   fi;
 
   # Concatenate all of the dtbs to the kernel
-  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+  if $NEW; then
+    cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+  else
+    cat $compressed_image /tmp/anykernel/dtbs/*.dtb-sar > /tmp/anykernel/Image.gz-dtb;
+  fi
 fi;
 
 write_boot;
